5018ffa96b98d8863c302fa6ab012ad4309986d1
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-03-19 17:40:31 +0530

    add k8s resources

---
+++ source/courses/learnk8s/code/docker-hello-world/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/version', (req, res) => {
+  res.send(process.env.VERSION || 'No version')
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
--- source/courses/learnk8s/index.rst
+++ source/courses/learnk8s/index.rst
@@ -1,3 +1,7 @@
 =====================================
 learnk8s.io Kubernetes Course
 =====================================
+
+This is a two-part course on learning Kubernetes from the
+ground up.
+
