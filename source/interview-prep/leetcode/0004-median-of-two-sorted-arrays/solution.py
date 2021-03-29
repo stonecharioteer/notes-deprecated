d5ca4c62cf6d43c8050dac0ef0176273b6ce0559
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-03-29 21:26:51 +0530

    add placeholder for LC0004

---
+++ source/interview-prep/leetcode/0004-median-of-two-sorted-arrays/solution.py
@@ -0,0 +1,8 @@
+"""Solution for Leetcode #0004 - Median of Two Sorted Arrays
+https://leetcode.com/problems/median-of-two-sorted-arrays/
+"""
+
+
+def median_two_sorted_arrays(nums1: list[int], nums2: list[int]) -> int:
+    """Return a median of two sorted arrays in O(log(m+n))
+    """
